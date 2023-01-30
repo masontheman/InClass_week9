56b367113c48e54879596868f88daaebc36b1807
mason delgado <masonman8787@gmail.com>
2023-01-30 15:28:16 -0600

    fourth commit

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -2,7 +2,7 @@ const MainAuthRouter = require('express').Router();
 
 MainAuthRouter.route('/register')
 .get((req,res) => {
-    res.send('Get Auth Credentials');
+    res.render('register');
 })
 .post((req,res) =>{
     res.send('Post Auth router Register');
