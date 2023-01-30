459c3ea34f5f8ef0a4fff2bd91e8a73c2390b883
mason delgado <masonman8787@gmail.com>
2023-01-30 15:09:11 -0600

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.set('view engine','ejs')
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'src/templates/views'));
 
 app.get('/',(req,res) =>{
-    res.render('register',{ username:'masond'});
+    res.send('hello World');
 });
 
 const initRoutes = require('./src/routes');
